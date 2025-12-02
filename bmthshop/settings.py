4fd924ac3d8412097b5aa208806c5ae3d2a16f32
NicoFerraro22 <nicolas.fe2202@gmail.com>
2025-12-02 20:18:10 -0300

    fix

--- bmthshop/settings.py
+++ bmthshop/settings.py
@@ -12,7 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")  
 DEBUG = os.environ.get("DJANGO_DEBUG", "0") == "1"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = ["*"]
+
 
 
 # ===========================
