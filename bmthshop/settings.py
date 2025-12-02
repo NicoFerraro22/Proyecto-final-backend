b8489b337dbd11ae999c557a3fc07cd1efa982c8
NicoFerraro22 <nicolas.fe2202@gmail.com>
2025-12-02 20:04:23 -0300

    arreglos

--- bmthshop/settings.py
+++ bmthshop/settings.py
@@ -35,6 +35,26 @@ INSTALLED_APPS = [
     "catalog",
 ]
 
+# ===========================
+# TEMPLATES (NECESARIO PARA ADMIN)
+# ===========================
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
 
 # ===========================
 # MIDDLEWARE
@@ -129,7 +149,6 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
-
 
 # ===========================
 # AUTO FIELD
